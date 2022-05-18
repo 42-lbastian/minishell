7ef03990b9ee3463ed880f8c7caee90ff7511ca2
Loic BASTIAN <lbastian@e1r2p15.clusters.42paris.fr>
2022-05-18 22:36:48 +0200

    Add change func to test

--- test/test.sh
+++ test/test.sh
@@ -16,47 +16,62 @@ function exec_cmd ()
 	fi
 }
 
-make re
+function exec_cmd_change ()
+{
+	echo $1 | ./minishell &> ../1
+	echo $2 | bash &> ../2
+	DIFF=$(diff ../1 ../2)
+	if [ "$DIFF" != "" ]
+	then
+		printf "${red}|%s| failed${normal}\n" "$1"
+	fi
+}
 clear
 
 ps -p $$
 printf "\n"
 
-#echo
-exec_cmd 'echo toto'
-exec_cmd 'echo toto tata titi'
-exec_cmd 'echo toto tata titi -n'
-exec_cmd 'echo toto'
-exec_cmd 'echo'
-exec_cmd 'echo "toto"'
-exec_cmd 'echo '\''toto'\'''
-exec_cmd 'echo " "'
-exec_cmd 'echo '\'' '\'''
-exec_cmd 'echo ""'
-exec_cmd 'echo ! "" !'
-exec_cmd 'echo '\'''\'''
-exec_cmd 'echo ! '\'''\'' !'
+if [ $# == 0 ]
+then
+	make re
+	exec_cmd 'echo toto'
+	exec_cmd 'echo toto tata titi'
+	exec_cmd 'echo toto tata titi -n'
+	exec_cmd 'echo toto'
+	exec_cmd 'echo'
+	exec_cmd 'echo "toto"'
+	exec_cmd 'echo '\''toto'\'''
+	exec_cmd 'echo " "'
+	exec_cmd 'echo '\'' '\'''
+	exec_cmd 'echo ""'
+	exec_cmd 'echo ! "" !'
+	exec_cmd 'echo '\'''\'''
+	exec_cmd 'echo ! '\'''\'' !'
 
-exec_cmd 'echo -n !toto tata titi!'
-exec_cmd 'echo -n !toto!'
-exec_cmd 'echo -n !""!'
-exec_cmd 'echo -n ! "" !'
-exec_cmd 'echo -n'
-
-
-exec_cmd 'echo "$USER"'
-exec_cmd 'echo '$USER''
-exec_cmd 'echo $USER'
-exec_cmd 'ls "$USER"'
-exec_cmd 'ls '$USER''
-exec_cmd '$USER'
-
-exec_cmd 'ls ~/Documents'
-exec_cmd 'ls ~/\Documents'
-exec_cmd 'ls ~/Docum\ents'
-exec_cmd 'ls \~/Documents'
+	exec_cmd 'echo -n !toto tata titi!'
+	exec_cmd 'echo -n !toto!'
+	exec_cmd 'echo -n !""!'
+	exec_cmd 'echo -n ! "" !'
+	exec_cmd 'echo -n'
 
 
+	exec_cmd "echo "\""$USER"\"""
+	exec_cmd 'echo '\''$USER'\'''
+	exec_cmd "echo $USER"
 
-printf "\n\n${magenta}A tester\n\n${normal}"
-echo 'ls ~/Documents/\'
+	exec_cmd "ls "\""$USER"\"""
+	exec_cmd "ls "\""$TOTO"\"""
+	exec_cmd 'ls '\''$USER'\'''
+
+	exec_cmd "$USER"
+	exec_cmd "$TOTO"
+else
+	printf "${magenta}Change\n\n${normal}"
+	exec_cmd_change 'ls ~/Documents/\' 'ls ~/Documents/'
+	exec_cmd_change 'ls ~/Docu\ments/' 'ls ~/Documents/'
+	exec_cmd_change 'ls \~/Documents/' 'ls ~/Documents/'
+	
+	echo
+	echo 'echo "to\"to"'
+	echo 'cat << EOF(what you want)'
+fi
