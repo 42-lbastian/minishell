8cc5b52addc543e7527bf5e61942dee104a0a8d0
Loic BASTIAN <lbastian@e1r12p23.clusters.42paris.fr>
2022-06-14 17:04:08 +0200

    Start parse tree

--- test/union.c
+++ test/union.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct s_node t_node;
 
 typedef struct	s_node_pointer
 {
+	char oper;
 	t_node	*left;
 	t_node	*right;
 
@@ -11,7 +13,7 @@ typedef struct	s_node_pointer
 
 typedef union	u_node_value
 {
-	char	c;
+	int	nb;
 	t_node_pointer node_pointer;
 }				t_union_node;
 
@@ -21,16 +23,29 @@ struct	s_node
 };
 
 
-
-int main()
+int main(int argc, char **argv)
 {
-	t_node node_a;
-	t_node node_b;
-	t_node node_p;
+	t_node *tab_node;
 	
-	node_a.value.c = 'a';
-	node_b.value.c = 'b';
+	tab_node = malloc(sizeof(t_node) * 5);
 
-	node_p.value.node_pointer.left = &node_a;
-	node_p.value.node_pointer.right = &node_b;
+	tab_node[0].value.nb = atoi(argv[1]);
+	tab_node[2].value.nb = atoi(argv[3]);
+	tab_node[4].value.nb = atoi(argv[5]);
+
+	tab_node[1].value.node_pointer.left = &tab_node[0];
+	tab_node[1].value.node_pointer.right = &tab_node[2];
+	tab_node[1].value.node_pointer.oper = argv[2][0];
+
+	tab_node[3].value.node_pointer.left = &tab_node[1];
+	tab_node[3].value.node_pointer.right = &tab_node[4];
+	tab_node[3].value.node_pointer.oper = argv[4][0];
+
+	printf("%d\t", tab_node[1].value.node_pointer.left->value.nb);
+	printf("%c\t", tab_node[1].value.node_pointer.oper);
+	printf("%d\t", tab_node[1].value.node_pointer.right->value.nb);
+	printf("%c\t", tab_node[3].value.node_pointer.oper);
+	printf("%d\n", tab_node[3].value.node_pointer.right->value.nb);
+
+	printf("%d\n", tab_node[3].value.node_pointer.left->value.node_pointer.left->value.nb);
 }
