29d1f310975486ea61ea834ffa177e1a86d524ab
hugo <hugo@spriet.tv>
2022-07-11 16:22:18 +0200

    update cd

--- minishell/builtins/cd.c
+++ minishell/builtins/cd.c
@@ -1,23 +1,44 @@
 #include "../include/minishell.h"
 
+char    *search_in_env
+
+
 void cd(t_List st, const char *path)
 {
     int c;
     char    cwd[1024];
     char *oldpath;
 
+
+    if (path == NULL || path[0] == '~')
+    {
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+        {
+            oldpath = ft_strjoin("OLDPWD=", cwd);
+            is_var(oldpath, st);
+        }
+        c = chdir("~");
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+        {
+            oldpath = ft_strjoin("PWD=", cwd);
+            is_var(oldpath, st);
+        }
+    } 
+    else
+    {
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+        {
+            oldpath = ft_strjoin("OLDPWD=", cwd);
+            is_var(oldpath, st);
+        }
+        c = chdir(path);
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+        {
+            oldpath = ft_strjoin("PWD=", cwd);
+            is_var(oldpath, st);
+        }
+        if (c != 0)
+           return ;
+    }   
     
-    if (getcwd(cwd, sizeof(cwd)) != NULL)
-    {
-        oldpath = ft_strjoin("OLDPWD=", cwd);
-        is_var(oldpath, st);
-    }
-    c = chdir(path);
-    if (getcwd(cwd, sizeof(cwd)) != NULL)
-    {
-         oldpath = ft_strjoin("PWD=", cwd);
-         is_var(oldpath, st);
-    }
-    if (c != 0)
-        return ;
 }
