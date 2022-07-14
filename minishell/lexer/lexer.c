2c1cf8b00d41a0074a284e3edfc5d55a0ff2ed02
Bastian <loic.bastian@gmail.com>
2022-07-14 17:00:07 +0200

    Fix spaces lexer

--- minishell/lexer/lexer.c
+++ minishell/lexer/lexer.c
@@ -1,76 +1,76 @@
 #include "../include/minishell.h"
 
-int	ft_lexer(char *str, t_struct *main)
+int	ft_lexer(char *str, t_struct *main_s)
 {
-	main->i = 0;
-	main->is_arg = 0;
-	while (str[main->i])
+	main_s->i = 0;
+	main_s->is_arg = 0;
+	while (str[main_s->i])
 	{
-		if (ft_belong_cmd_start(str[main->i]) && main->is_arg == 0)
-			ft_read_cmd(str, main);
-		if (str[main->i] == '-')
-			ft_read_flag(str, main);
-		if (ft_belong_cmd_start(str[main->i]) && main->is_arg == 1)
-			ft_read_arg(str, main);
-		if (str[main->i] == '"')
-			ft_read_quotes(str, main, '"');
-		if (str[main->i] == '\'')
-			ft_read_quotes(str, main, '\'');
-		if (ft_special_char(str[main->i]))
-			ft_read_special(str, main);
-		while (str[main->i] == ' ' && str[main->i])
-			main->i++;
-		if (main->char_check.error == 1)
+		if (ft_belong_cmd_start(str[main_s->i]) && main_s->is_arg == 0)
+			ft_read_cmd(str, main_s);
+		if (str[main_s->i] == '-')
+			ft_read_flag(str, main_s);
+		if (ft_belong_cmd_start(str[main_s->i]) && main_s->is_arg == 1)
+			ft_read_arg(str, main_s);
+		if (str[main_s->i] == '"')
+			ft_read_quotes(str, main_s, '"');
+		if (str[main_s->i] == '\'')
+			ft_read_quotes(str, main_s, '\'');
+		if (ft_special_char(str[main_s->i]))
+			ft_read_special(str, main_s);
+		while (str[main_s->i] == ' ' && str[main_s->i])
+			main_s->i++;
+		if (main_s->char_check.error == 1)
 			return (1);
 	}
 	return (0);
 }
 
-int	ft_main_lexer(char *str, t_struct *main)
+int	ft_main_s_lexer(char *str, t_struct *main_s)
 {	
 	int		ret;
 
-	str = ft_check_quotes(str, main);
-	str = ft_check_quotes(str, main);
-	str = ft_remove_special(str, main, 0);
-	ret = ft_lexer(str, main);
-	ft_remove_spaces(&main->lst);
+	str = ft_check_quotes(str, main_s);
+	str = ft_check_quotes(str, main_s);
+	str = ft_remove_special(str, main_s, 0);
+	ret = ft_lexer(str, main_s);
+	ft_remove_spaces(&main_s->lst);
 	free(str);
 	return (ret);
 }
 
-void	ft_cmd(t_struct *main, t_List st)
+void	ft_cmd(t_struct *main_s, t_List st)
 {
-	if (ft_strcmp_2(main->temp_str[0], "echo") == 0)
-		echo(main->temp_str);
-	if (ft_strcmp_2(main->temp_str[0], "cd") == 0)
-		cd(st, main->temp_str[1]);
-	if (ft_strcmp_2(main->temp_str[0], "pwd") == 0)
+	if (ft_strcmp_2(main_s->temp_str[0], "echo") == 0)
+		echo(main_s->temp_str);
+	if (ft_strcmp_2(main_s->temp_str[0], "cd") == 0)
+		cd(st, main_s->temp_str[1]);
+	if (ft_strcmp_2(main_s->temp_str[0], "pwd") == 0)
 		pwd();	
-	if (ft_strcmp_2(main->temp_str[0], "env") == 0)
+	if (ft_strcmp_2(main_s->temp_str[0], "env") == 0)
 		print_env(st);
-	if (ft_strcmp_2(main->temp_str[0], "export") == 0)
-		ft_export(st, main->temp_str);
+	if (ft_strcmp_2(main_s->temp_str[0], "export") == 0)
+		ft_export(st, main_s->temp_str);
 }
 
-void	ft_temp_test_cmd(t_struct *main)
+void	ft_temp_test_cmd(t_struct *main_s)
 {
 	int i;
 	int size;
 
 
-	size = ft_lst_size(main->lst);
+	size = ft_lst_size(main_s->lst);
 	i = 0;
-	main->temp_str = malloc(sizeof(char*) * (size + 1));
-	main->temp_str[size] = NULL;
+	main_s->temp_str = malloc(sizeof(char*) * (size + 1));
+	main_s->temp_str[size] = NULL;
 	while (i < size)
 	{
-		main->temp_str[i] = ft_strcpy_2(ft_get_lst_str_index(main->lst, i));
+		main_s->temp_str[i] = ft_strcpy_2(ft_get_lst_str_index(main_s->lst, i));
 		i++;
 	}
 }
 
-int	ft_main_action(t_struct *main, char *str_read, t_List st)
+int	ft_main_s_action(t_struct *main_s, char *str_read, t_List st)
 {
 	while (1)
 	{
@@ -82,16 +82,16 @@ int	ft_main_action(t_struct *main, char *str_read, t_List st)
 			break ;
 		if (ft_strlen(str_read) != 0)
 			add_history(str_read);
-		if (ft_main_lexer(str_read, main))
+		if (ft_main_s_lexer(str_read, main_s))
 		{
-			ft_free_lst(&main->lst);
+			ft_free_lst(&main_s->lst);
 			break ;
 		}
 		//parser
-		ft_temp_test_cmd(main);
-		ft_cmd(main, st);
-		ft_print_lst(main->lst);
-		ft_free_lst(&main->lst);
+		ft_temp_test_cmd(main_s);
+		ft_cmd(main_s, st);
+		ft_print_lst(main_s->lst);
+		ft_free_lst(&main_s->lst);
 	}
 	free(str_read);
 	return (0);
@@ -100,18 +100,18 @@ int	ft_main_action(t_struct *main, char *str_read, t_List st)
 int	main(int argc, char **argv, char **envp)
 {
 	char		*str_read;
-	t_struct	*main;
+	t_struct	*main_s;
 	t_List st;
 
     st = NULL;
     st = add_list(envp, st);
-	main = malloc(sizeof(t_struct));
-	if (!main)
+	main_s = malloc(sizeof(t_struct));
+	if (!main_s)
 		return (1);
 	str_read = NULL;
-	ft_init_struct(main, argc, argv);
-	ft_main_action(main, str_read, st);
-	free(main);
+	ft_init_struct(main_s, argc, argv);
+	ft_main_s_action(main_s, str_read, st);
+	free(main_s);
 	clear_history();
 	return (0);
 }
--- minishell/lexer/remove_spaces.c
+++ minishell/lexer/remove_spaces.c
@@ -1,21 +1,59 @@
 #include "../include/minishell.h"
 
-int		ft_count_spaces_flag(char *str)
+int		ft_count_spaces(char *str)
 {
 	int	size;
 	int	i;
 
 	size = 0;
 	i = 0;
-	
+	while (str[i])
+	{
+		while (str[i] && str[i] != ' ')
+		{
+			i++;
+			size++;
+		}
+		size++;
+		while (str[i] && str[i] == ' ')
+			i++;
+	}
+	size--;
 	return (size);
 }
 
-void	ft_spaces_flag(char *str)
+char	*ft_spaces_flag(char *str)
 {
-	int size;
+	int		i;
+	int		j;
+	int		size;
+	char	*temp;
 
-	size = ft_count_spaces_flag(str);
+	i = 0;
+	j = 0;
+	size = ft_count_spaces(str);
+	temp = malloc(sizeof(char) * (size + 1));
+	temp[size] = '\0';
+	if (!temp)
+		return (NULL);
+	while (str[i])
+	{
+		while (str[i] && str[i] != ' ')
+		{
+			temp[j] = str[i];
+			j++;
+			i++;
+		}
+		if (str[i] && j < size)
+		{
+			j++;
+			temp[i] = ' ';
+		}
+		while (str[i] && str[i] == ' ')
+			i++;
+	}
+	free(str);
+	return (temp);
 }
 
 void	ft_remove_spaces(t_list **lst)
@@ -26,8 +64,9 @@ void	ft_remove_spaces(t_list **lst)
 	while (lst && (*lst))
 	{
 		if ((*lst)->type == FLAG)
-			ft_spaces_flag((*lst)->content);
-		//if ((*lst)->type == ARG)
+			(*lst)->content = ft_spaces_flag((*lst)->content);
+		if ((*lst)->type == ARG && ((*lst)->content[0] != '"' && (*lst)->content[0] != '\''))
+			(*lst)->content = ft_spaces_flag((*lst)->content);
 		(*lst) = (*lst)->next;
 	}
 	(*lst) = temp;
