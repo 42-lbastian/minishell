c0f87f8ad720a9f994b71300fdfa63a957a49a62
Stephen ELIE <stelie@student.42.fr>
2022-11-17 10:03:10 +0100

    almost norm OK .h

--- minishell/include/minishell.h
+++ minishell/include/minishell.h
@@ -1,83 +1,81 @@
 #ifndef MINISHELL_H
-#define MINISHELL_H
+# define MINISHELL_H
 
-#define RED "\1\033[1;31m\2"
-#define NORMAL "\1\x1b[0m\2"
-#define NAME "minishell> "
+# define RED "\1\033[1;31m\2"
+# define NORMAL "\1\x1b[0m\2"
+# define NAME "minishell> "
 
-#define NB_CHAR_VALID 13
+# define NB_CHAR_VALID 13
 
-#define SIMPLE 0
-#define DOUBLE 1
+# define SIMPLE 0
+# define DOUBLE 1
 
-#define	DEFAULT 1
-#define CMD 2
-#define ARG_FILE_IN 3
-#define ARG_FILE_OUT_OVER 4
-#define ARG_FILE_OUT_APP 5
-#define LIMITOR 6
-#define PIPE 7 /* | */
-#define FILE_IN 8 /* < */
-#define FILE_OUT_OVER 9 /* > */
-#define FILE_OUT_APP 10 /* >> */
-#define HERE_DOC 11 /* << */
+# define DEFAULT 1
+# define CMD 2
+# define ARG_FILE_IN 3
+# define ARG_FILE_OUT_OVER 4
+# define ARG_FILE_OUT_APP 5
+# define LIMITOR 6
+# define PIPE 7 /* | */
+# define FILE_IN 8 /* < */
+# define FILE_OUT_OVER 9 /* > */
+# define FILE_OUT_APP 10 /* >> */
+# define HERE_DOC 11 /* << */
 
-#define CMD_BEGIN 0
-#define CMD_END 1
-#define CMD_MIDDLE 2
-#define CMD_FILE_IN 3
-#define CMD_FILE_IN_END 4
-#define CMD_FILE_OUT 5
-#define CMD_FILE_OUT_END 6
+# define CMD_BEGIN 0
+# define CMD_END 1
+# define CMD_MIDDLE 2
+# define CMD_FILE_IN 3
+# define CMD_FILE_IN_END 4
+# define CMD_FILE_OUT 5
+# define CMD_FILE_OUT_END 6
 
+# define CD 0
+# define ECHO 1
 
-#define CD 0
-#define ECHO 1
-
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <signal.h>
-#include <sys/wait.h>
-#include "../libft/includes/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <sys/wait.h>
+# include "../libft/includes/libft.h"
 
 typedef struct s_env
 {
-	char			    	*var;
-    char                    *value;
-	struct s_env			*next;
+	char			*var;
+	char			*value;
+	struct s_env	*next;
 }				t_env;
 
-
 //Value for AST/LST_CMD
-typedef union	s_value
+typedef union s_value
 {
 	char	**cmd;
 	char	*oper;
 }				t_value;
 
 //LST_CMD
-typedef struct	s_lst_cmd
+typedef struct s_lst_cmd
 {
 	struct s_lst_cmd	*next;
 	struct s_lst_cmd	*prev;
-	t_value			value;
-	int				type;
+	t_value				value;
+	int					type;
 
 }				t_lst_parser;
 
 //LEXER TOKEN
-typedef struct	s_list
+typedef struct s_list
 {
 	char			*content;
 	struct s_list	*next;
 	int				type;
 }				t_list;
 
-typedef	struct	s_char_check
+typedef struct s_char_check
 {
 	int		last_simple_q;
 	int		last_double_q;
@@ -97,49 +95,48 @@ typedef struct s_struct
 //SIGNAL + RET FUNC
 typedef struct s_sig
 {
-	int				ret;
+	int			ret;
 	int			sigint;
 	int			sigquit;
 	pid_t		pid;
-}		t_sig;
+}				t_sig;
 
-extern	t_sig g_glob;
+extern t_sig	g_glob;
 
 /**
 **		signal/signal.c
 **/
 void	get_signal(int sig);
 
-
 /**
 **		lexer/lexer.c
 **/
-int	ft_main_lexer(char *str, t_struct *main_s, t_env *st);
+int		ft_main_lexer(char *str, t_struct *main_s, t_env *st);
 
 /**
 **		lexer/create_env.c
 **/
-int	ft_create_env(char **envp, t_env **st);
+int		ft_create_env(char **envp, t_env **st);
 
 /**
 **		lexer/create_env_lst.c
 **/
 t_env	*ft_lst_new_env(char *var, char *value);
-int	ft_lst_add_back_env(t_env **st, t_env *new);
+int		ft_lst_add_back_env(t_env **st, t_env *new);
 
 /*		
 **		lexer/char_check.c
 */
-int	ft_belong_cmd_start(char c);
-int	ft_belong_cmd_end(char c);
-int	ft_is_alpha_numb(char c);
-int	ft_count_check(char c);
+int		ft_belong_cmd_start(char c);
+int		ft_belong_cmd_end(char c);
+int		ft_is_alpha_numb(char c);
+int		ft_count_check(char c);
 
 /*
 **		lexer/char_check_special.c
 */
-int	ft_special_char(char c);
-int	ft_exclude_special(char c, t_struct *main);
+int		ft_special_char(char c);
+int		ft_exclude_special(char c, t_struct *main);
 
 /*
 **		lexer/init.c
@@ -150,21 +147,21 @@ void	ft_fill_tab_char(t_struct *main, char *str);
 /*
 **		lexer/remove_quotes.c
 */
-int	ft_main_remove_quotes(t_list **lst);
+int		ft_main_remove_quotes(t_list **lst);
 
 /*
 **		lexer/expand_var.c
 */
-int	ft_main_replace_env(t_list **lst, t_env *st);
+int		ft_main_replace_env(t_list **lst, t_env *st);
 
 /**
 **		lexer/count.c
 **/
-int	ft_start_read_var(char *str, int index);
-int	ft_lenght_read_var(char *str, int index);
-int	ft_count_char(char *str);
-int	ft_count_nb_quotes(char *str);
-int	ft_count_remove_quotes(char *str);
+int		ft_start_read_var(char *str, int index);
+int		ft_lenght_read_var(char *str, int index);
+int		ft_count_char(char *str);
+int		ft_count_nb_quotes(char *str);
+int		ft_count_remove_quotes(char *str);
 
 /*
 **		lexer/utils.c
@@ -191,8 +188,8 @@ int		ft_count_read_quotes(char *str, int start);
 /*
 **		lexer/unwanted.c
 */
-char *ft_check_quotes(char *str, t_struct *main);
-char *ft_remove_special(char *str, t_struct *main, int i);
+char	*ft_check_quotes(char *str, t_struct *main);
+char	*ft_remove_special(char *str, t_struct *main, int i);
 
 /*
 **		lexer/lst.c
@@ -207,9 +204,8 @@ char	*ft_find_var(char *str, t_env *st);
 */
 t_list	*ft_lst_new_join(char *content, int type);
 t_list	*ft_lst_new(char *content, int type);
-t_list *ft_lst_last(t_list *lst);
+t_list	*ft_lst_last(t_list *lst);
 int		ft_lstadd_back(t_list **lst, t_list *new);
-
 
 /*
 **		lexer/lst_print.c
@@ -231,9 +227,7 @@ int		ft_remove_spaces(t_list **lst);
 /*
 **		lexer/set_type_cmd.c
 */
-int	ft_set_type_cmd(t_list **lst);
-
-
+int		ft_set_type_cmd(t_list **lst);
 
 /*
 **		parse/parse.c
@@ -252,8 +246,8 @@ void	ft_is_builtin_dumb(char **complete_cmd, t_env *st, int read, int write, int
 **		parse/lst.c
 */
 t_lst_parser	*ft_lst_parse_new(char **cmd, char *oper, int type);
-int	ft_lst_parse_add_back(t_lst_parser **lst, t_lst_parser *new);
-int	ft_lst_parse_add_front(t_lst_parser **lst, t_lst_parser *new);
+int		ft_lst_parse_add_back(t_lst_parser **lst, t_lst_parser *new);
+int		ft_lst_parse_add_front(t_lst_parser **lst, t_lst_parser *new);
 void	ft_print_lst_parse(t_lst_parser *lst);
 void	ft_print_lst_parse_reverse(t_lst_parser *lst);
 char	*ft_find_var_path(char *str, t_env *st);
