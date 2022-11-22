2b9dda61678377f33cef5c4df6cf5691127bdd85
Stephen ELIE <stelie@student.42.fr>
2022-11-22 12:50:05 +0100

    removed old version functions

--- minishell/main/signal.c
+++ minishell/main/signal.c
@@ -3,7 +3,7 @@
 /*
  * @brief handle the CTRL+C signal for the main process
  * @param sig: the signal (here SIGINT)
- * @todo set the error code (130)
+ * @todo set the error code (130 for CTRL+C)
  */
 void	global_signal(int sig)
 {
@@ -25,49 +25,3 @@ void	global_signals_handler(void)
 	signal(SIGINT, SIG_IGN);
 	signal(SIGINT, global_signal);
 }
-
-/*
- * @brief: old signal handler
- * @author: hspriet
- * 
- *
- void	parent_signal(int sig)
-{
-	if (sig == SIGQUIT)
-	{
-		ft_putstr_fd("\b\b  \b\b", 2);
-	}
-	if (sig == SIGINT)
-	{
-		ft_putstr_fd("\n", 1);
-		rl_replace_line("", 1);
-		rl_on_new_line();
-		rl_redisplay();
-		g_glob.ret = 130;
-	}
-}
-
-void	child_signal(int sig)
-{
-	if (sig == SIGQUIT)
-	{
-		ft_putstr_fd("Quit: (core dumped)\n", 2);
-		g_glob.ret = 131;
-		g_glob.sigquit = 1;
-	}
-	else if (sig == SIGINT)
-	{
-		ft_putstr_fd("\n", 2);
-		g_glob.ret = 130;
-		g_glob.sigint = 1;
-	}
-}
-
-void	get_signal(int sig)
-{
-	if (g_glob.pid)
-		child_signal(sig);
-	else
-		parent_signal(sig);
-}
-*/
