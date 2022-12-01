65cf6ac078527f5698e51ede70f7573b0e1ae4a9
Loic BASTIAN <lbastian@e1r2p13.clusters.42paris.fr>
2022-12-01 20:03:39 +0100

    Fix fork exit && cd

--- minishell/parse/exec.c
+++ minishell/parse/exec.c
@@ -6,7 +6,7 @@
 /*   By: stelie <stelie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 15:47:29 by lbastian          #+#    #+#             */
-/*   Updated: 2022/12/01 18:39:57 by lbastian         ###   ########.fr       */
+/*   Updated: 2022/12/01 20:02:15 by lbastian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,15 +54,22 @@ static void	ms_exec_builtin_loop(char **complete_cmd)
 {
 	if (ft_strcmp(complete_cmd[0], "echo") == 0)
 		echo_builtin(complete_cmd);
-	if (ft_strcmp(complete_cmd[0], "cd") == 0)
-		cd_builtin(complete_cmd);
 	if (ft_strcmp(complete_cmd[0], "pwd") == 0)
 		pwd_builtin();
-	if (ft_strcmp(complete_cmd[0], "exit") == 0)
-		exit_builtin(complete_cmd);
 }
 
-static int	ms_exec_builtin_short(char **complete_cmd, int pip[2][2], int type)
+static int	ms_exec_builtin_env(char **complete_cmd, int pip[2][2], int type)
+{
+	ms_close_fd_parent(pip, type);
+	if (ft_strcmp(complete_cmd[0], "cd") == 0)
+		set_err_code (cd_builtin(complete_cmd));
+	if (ft_strcmp(complete_cmd[0], "exit") == 0)
+		exit_builtin(complete_cmd);
+	return (EXIT_SUCCESS);
+	
+}
+
+static int	ms_exec_builtin_fork(char **complete_cmd, int pip[2][2], int type)
 {
 	int		pid;
 	int		status;
@@ -82,6 +89,8 @@ static int	ms_exec_builtin_short(char **complete_cmd, int pip[2][2], int type)
 		set_err_code(0);
 		ms_close_fd_parent(pip, type);
 		waitpid(pid, &status, 0);
+		set_err_code(WEXITSTATUS(status));
+
 	}
 	return (EXIT_SUCCESS);
 }
@@ -89,13 +98,13 @@ static int	ms_exec_builtin_short(char **complete_cmd, int pip[2][2], int type)
 int	ms_is_builtin_short(char **complete_cmd, t_env *st, int pip[2][2], int type)
 {
 	if (ft_strcmp(complete_cmd[0], "cd") == 0)
-		return (ms_exec_builtin_short(complete_cmd, pip, type));
+		return (ms_exec_builtin_env(complete_cmd, pip, type));
 	else if (ft_strcmp(complete_cmd[0], "exit") == 0)
-		return (ms_exec_builtin_short(complete_cmd, pip, type));
+		return (ms_exec_builtin_env(complete_cmd, pip, type));
 	else if (ft_strcmp(complete_cmd[0], "echo") == 0)
-		return (ms_exec_builtin_short(complete_cmd, pip, type));
+		return (ms_exec_builtin_fork(complete_cmd, pip, type));
 	else if (ft_strcmp(complete_cmd[0], "pwd") == 0)
-		return (ms_exec_builtin_short(complete_cmd, pip, type));
+		return (ms_exec_builtin_fork(complete_cmd, pip, type));
 	else
 		return (ms_main_exec_short(complete_cmd, st, pip, type));
 }
