a8d55f27c09f27d02059433c4e71c0c9a9983651
Stephen ELIE <stelie@student.42.fr>
2022-12-05 17:42:22 +0100

    added spaces

--- minishell/includes/ms_builtins.h
+++ minishell/includes/ms_builtins.h
@@ -6,7 +6,7 @@
 /*   By: stelie <stelie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 13:46:17 by stelie            #+#    #+#             */
-/*   Updated: 2022/12/05 17:26:15 by stelie           ###   ########.fr       */
+/*   Updated: 2022/12/05 17:39:13 by stelie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@
 
 # define ERR_CD_ARGS "cd: too many arguments\n"
 # define ERR_HOME_NOT_SET "cd: HOME not set\n"
+
 # define ERR_ENV_PERM ": Premission denied\n"
 # define ERR_ENV_NO_SUCH ": No such file or directory\n"
+
 # define ERR_EXIT_VALID ": numeric argument required\n"
 # define ERR_EXIT_ARGS "exit : too many arguments\n"
+
 # define ERR_EXPORT "\': not a valid identifier\n"
 # define EXIT_MSG "exit\n"
 
