8f377122994b7541be19f71d7d92f16c3f125352
Stephen ELIE <stelie@student.42.fr>
2022-12-05 17:30:53 +0100

    corrected exit_builtin, errmsg fix + fsanitized

--- minishell/builtins/exit_builtin.c
+++ minishell/builtins/exit_builtin.c
@@ -6,11 +6,30 @@
 /*   By: stelie <stelie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 10:31:13 by stelie            #+#    #+#             */
-/*   Updated: 2022/12/02 13:43:08 by stelie           ###   ########.fr       */
+/*   Updated: 2022/12/05 17:28:40 by stelie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+/*
+ * @brief Displays the adequate error msg if exit is not used correctly.
+*/
+static int	_display_exit_error(char *arg, char *error, int exit_code)
+{
+	if (ft_strcmp(error, ERR_EXIT_ARGS) == 0)
+	{
+		ft_putstr_fd(EXIT_MSG, STDERR_FILENO);
+		ft_putstr_fd(error, STDERR_FILENO);
+	}
+	else
+	{
+		ft_putstr_fd("exit: ", STDERR_FILENO);
+		ft_putstr_fd(arg, STDERR_FILENO);
+		ft_putstr_fd(error, STDERR_FILENO);
+	}
+	return (exit_code);
+}
 
 static bool	_is_valid_exit_flag(char *arg)
 {
@@ -43,23 +62,15 @@ int	exit_builtin(char **args)
 	int	exit_code;
 
 	exit_code = EXIT_SUCCESS;
-	if (ft_strcmp(args[0], "exit") != 0)
-		return (EXIT_FAILURE);
-	if (args[1])
-		exit_code = ft_atoi(args[1]);
+	if (args[1] == NULL)
+		exit(get_err_code());
+	else if (args[2] != NULL)
+		return (_display_exit_error(args[1], ERR_EXIT_ARGS, EXIT_FAILURE));
+	else if (_is_valid_exit_flag(args[1]) == false)
+		exit_code = _display_exit_error(args[1], ERR_EXIT_VALID, 2);
 	else
-		exit_code = get_err_code();
-	if (_is_valid_exit_flag(args[1]) == false)
-	{
-		printf("exit: %s: %s", args[1], ERR_EXIT_VALID);
-		exit_code = 2;
-	}
-	else if (args[1] && args[2])
-	{
-		ft_putstr_fd(ERR_EXIT_ARGS, STDERR_FILENO);
-		return (ft_putmsg_fd("exit\n", STDERR_FILENO, EXIT_FAILURE));
-	}
-	ft_putstr_fd("exit\n", STDERR_FILENO);
+		exit_code = ft_atoi(args[1]);
+	ft_putstr_fd(EXIT_MSG, STDERR_FILENO);
 	exit(exit_code);
 	return (exit_code);
 }
